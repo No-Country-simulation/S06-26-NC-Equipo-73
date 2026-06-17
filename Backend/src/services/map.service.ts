8d9976f3dec90d6c748d19ea86eebf2c2e9450ba
keinlinks <kevin.clift.buzeta@gmail.com>
2026-06-17 19:57:25 -0400

    feat: created map controller

---
+++ Backend/src/services/map.service.ts
@@ -0,0 +1,20 @@
+export interface MapRegion {
+    region: string;
+    lat: number;
+    lng: number;
+    concentration: number;
+    networkCoverage: number;
+    indicators: string[];
+}
+
+export interface MapResponse {
+    regions: MapRegion[];
+}
+
+export class MapService {
+    getRegions(): MapResponse {
+        return {
+            regions: [],
+        };
+    }
+}
