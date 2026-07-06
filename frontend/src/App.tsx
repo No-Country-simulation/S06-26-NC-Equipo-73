f4587949d0ab4907f3a01d024bdf828b36d198a7
feichulin <feimosqueda777@gmail.com>
2026-07-06 17:07:39 -0300

    cambio el navbar de posicion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Outlet />
     </>
   );
